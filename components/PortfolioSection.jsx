87e4e96b68552a1e5a624a633d1b2100af061343
Farrukh Zeb <FarrukhXeb@gmail.com>
2021-07-20 01:13:28 +0500

    Added dynamic data

--- components/PortfolioSection.jsx
+++ components/PortfolioSection.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
 
-const PorfolioSection = () => {
+const PorfolioSection = ({ portfolios }) => {
   return (
     <div className="wpo-protfolio-area-5 section-padding">
       <div className="container">
@@ -14,47 +14,35 @@ const PorfolioSection = () => {
           <div className="col-12">
             <div className="wpo-protfolio-item">
               <div className="row">
-                <div className="col col-12">
-                  <div className="protfolio-wrapper">
-                    <div className="wpo-protfolio-single">
-                      <div className="wpo-protfolio-img">
-                        <img src="images/protfolio/img-13.jpg" alt="" />
-                      </div>
-                      <div className="wpo-protfolio-text">
-                        <h2>Website Development</h2>
-                        <span>WordPress . Blog</span>
-                        <Link href="/PorfolioGrid4">
-                          <a>View Work</a>
-                        </Link>
-                      </div>
-                      <div className="thumb-text">
-                        <span>01</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col col-12">
-                  <div className="protfolio-rwrapper">
-                    <div className="wpo-protfolio-single">
-                      <div className="wpo-protfolio-img">
-                        <img src="images/protfolio/img-14.jpg" alt="" />
-                      </div>
-                      <div className="wpo-protfolio-text">
-                        <h2>Website Development</h2>
-                        <span>WordPress . Blog</span>
-                        <Link href="/PorfolioGrid4">
-                          <a>View Work</a>
-                        </Link>
-                      </div>
-                      <div className="thumb-text">
-                        <span>02</span>
+                {portfolios.map((portfolio) => (
+                  <div key={portfolio.id} className="col col-12">
+                    <div className="protfolio-rwrapper">
+                      <div className="wpo-protfolio-single">
+                        <div className="wpo-protfolio-img">
+                          {/* <img src="images/protfolio/img-14.jpg" alt="" /> */}
+                          <img
+                            style={{ width: "100%", height: 470 }}
+                            src={portfolio.coverImage.url}
+                            alt=""
+                          />
+                        </div>
+                        <div className="wpo-protfolio-text">
+                          <h2>{portfolio.title}</h2>
+                          <span>{portfolio.tags.join(" . ")}</span>
+                          <Link href={`/porfolio/${portfolio.slug}`}>
+                            <a>View Work</a>
+                          </Link>
+                        </div>
+                        {/* <div className="thumb-text">
+                          <span>02</span>
+                        </div> */}
                       </div>
                     </div>
                   </div>
-                </div>
+                ))}
               </div>
               <div className="protfolio-btn">
-                <Link href="/PorfolioGrid4">
+                <Link href="/porfolio">
                   <a>See More Work...</a>
                 </Link>
               </div>
--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,24 @@ import Hero from "../components/Hero";
 import PorfolioSection from "../components/PortfolioSection";
 import ServiceSection from "../components/ServiceSection";
 import TestimonialSlider from "../components/TestimonialSlider";
+import { getPostsAndPortfolio } from "../lib/data";
 
-// import { getPostsAndPortfolio } from "../lib/data";
+export const getServerSideProps = async () => {
+  const data = await getPostsAndPortfolio();
+  return {
+    props: {
+      data,
+    },
+  };
+};
 
-// export const getServerSideProps = async () => {
-//   const data = await getPostsAndPortfolio();
-//   return {
-//     props: {
-//       data,
-//     },
-//   };
-// };
-
-export default function Home() {
+export default function Home({ data }) {
   return (
     <>
       <Hero />
       <AboutArea ab3class={"wpo-about-area-4"} />
       <ServiceSection serviceclass={"wpo-service-area-5 section-padding"} />
-      <PorfolioSection />
+      <PorfolioSection portfolios={data.portfolios} />
       <TestimonialSlider testclass={"wpo-testimonial-area-5"} />
       <ContactArea contactclass={"wpo-contact-area5"} />
     </>
