c4a2c7eaa616c618bb3cccb6e23e6826e427f66e
Farrukh Zeb <FarrukhXeb@gmail.com>
2021-07-19 19:22:13 +0500

    clean up commits

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
-import "../styles/globals.css";
+// import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+
+import Head from "next/head";
 
 import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Farrukh | Lets get to Coding</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
